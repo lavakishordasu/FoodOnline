31513028458aca6faa3d0f404ad55fe093115722
lavakishordasu <137041721+lavakishordasu@users.noreply.github.com>
2024-08-13 14:55:44 +0530

    Update settings.py

--- FoodOnline_main/settings.py
+++ FoodOnline_main/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["3.15.140.117"]
+
 
 
 # Application definition
